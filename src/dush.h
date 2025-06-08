240567d29a9aac9231d362f9b28284e5ab08727e
NicknEma <emanuele.rovini@gmail.com>
2025-06-08 16:54:40 +0200

    Add some forward declarations

--- src/dush.h
+++ src/dush.h
@@ -1,7 +1,10 @@
 #ifndef DUSH_H
 #define DUSH_H
 
+static void init_ctrl_c_handler(void);
+
 static String get_line(Arena *arena);
 static String get_current_directory(Arena *arena);
+static String get_system_path(Arena *arena);
 
 #endif
