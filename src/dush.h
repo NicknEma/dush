b19eae4e6979eb0d5a01ef9717b21b45ee071628
NicknEma <emanuele.rovini@gmail.com>
2025-06-09 23:20:55 +0200

    Write actual help text

--- src/dush.h
+++ src/dush.h
@@ -1,6 +1,13 @@
 #ifndef DUSH_H
 #define DUSH_H
 
+#define HELP_TEXT \
+"The dush shell has a minimal set of commands:\n" \
+"  cd  \tPrints or sets the current directory\n" \
+"  exit\tExits the shell\n" \
+"  help\tPrints this text\n" \
+"  pwd \tPrints the current directory\n"
+
 static void init_ctrl_c_handler(void);
 
 static String get_line(Arena *arena);
